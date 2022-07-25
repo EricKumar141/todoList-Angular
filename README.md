960a708998863884733594bcd7e5583a0eb1c17a
EricKumar141 <eric_kumar@student.uml.edu>
2022-07-25 00:52:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-czl1w5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-czl1w5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  todoList: any[] = [];
+  addTask(item: string) {
+    this.todoList.push({ id: this.todoList.length, name: item });
+    console.log(this.todoList);
+  }
+  deleteTask(id: number) {
+    this.todoList = this.todoList.filter((item) => item.id !== id);
+  }
+}
